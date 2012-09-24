f5333772e487f85cf0fcf50d232d6ca32f7c0086
Nathan Calies <ncalies@gmail.com>
2012-09-24 15:36:20 -0700

    added tests

--- pantlegs.rb
+++ pantlegs.rb
@@ -1,4 +1,4 @@
-class PantLegs
+class PantLeg
 
  	def initialize(wind_dir, travel_dir, temp)
  		@wind_direction = wind_dir.upcase
@@ -24,11 +24,36 @@ class PantLegs
 end
 
 #using the puts below to test until I learn how to write a test
+#goal: riding.roll_up? must be true, otherwise fail
 if __FILE__ == $0
-  riding = PantLegs.new "W", "N", 65
-  puts "Should I roll up my pant leg? #{riding.roll_up?}"
-  riding = PantLegs.new "N", "N", 65
-  puts "Should I roll up my pant leg? #{riding.roll_up?}"
-  riding = PantLegs.new "E", "N", 49
-  puts "Should I roll up my pant leg? #{riding.roll_up?}"
+  require 'minitest/autorun'
+
+  describe PantLeg do
+    describe "#roll_up?" do
+    
+      describe "when initialized w/good values" do
+        let(:pantleg) { PantLeg.new "W", "N", 65 }
+
+        it "returns true" do
+          pantleg.roll_up?.must_equal true
+        end
+      end
+
+      describe "when initialized w/bad values" do
+        let(:pantleg) { PantLeg.new "N", "N", 30 }
+
+        it "returns false" do
+          pantleg.roll_up?.must_equal false
+        end
+      end
+
+    end
+  end
+
+  # riding = PantLeg.new "W", "N", 65
+  # puts "Should I roll up my pant leg? #{riding.roll_up?}"
+  # riding = PantLeg.new "N", "N", 65
+  # puts "Should I roll up my pant leg? #{riding.roll_up?}"
+  # riding = PantLeg.new "E", "N", 49
+  # puts "Should I roll up my pant leg? #{riding.roll_up?}"
 end
